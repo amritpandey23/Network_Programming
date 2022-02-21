94cbcb09cfece059e26b22ed1a15c2d9910e5b74
Amrit Pandey <amritpandey@tuta.io>
2022-02-22 00:16:26 +0530

    Assigment 5 - Redone

--- 207907_Amrit_Pandey_MCA_CCN_Assignment5/udp_client.py
+++ 207907_Amrit_Pandey_MCA_CCN_Assignment5/udp_client.py
@@ -2,10 +2,17 @@
 # Write a Python Program to Send a Large UDP Packet from Server to Client using
 # Fragmentation
 
+# id
+# offset
+# mf
+# header len
+# total len
+
 import socket
 
 server = "localhost"
 port = 9090
+mtu = 200
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -13,10 +20,15 @@ msg = "get data".encode('utf-8')
 
 client.sendto(msg, (server,port))
 
-msg, server = client.recvfrom(512)
+msg, server = client.recvfrom(mtu)
+id, offset, mf, hl, tl, data = msg.decode('utf-8').split(';')
 
-while msg != 'over':
-    print(msg)
-    msg, server = client.recvfrom(512)
-    msg = msg.decode('utf-8')
+while True:
+    print(f"ID:{id}\nOFFSET:{offset}\nMF:{mf}\nHL:{hl}\nTL:{tl}\nDATA:{data}")
+    msg, server = client.recvfrom(mtu)
+    id, offset, mf, hl, tl, data = msg.decode('utf-8').split(';')
+    if mf == '0':
+        print(f"ID:{id}\nOFFSET:{offset}\nMF:{mf}\nHL:{hl}\nTL:{tl}\nDATA:{data}")
+        break
+
 
--- 207907_Amrit_Pandey_MCA_CCN_Assignment5/udp_server.py
+++ 207907_Amrit_Pandey_MCA_CCN_Assignment5/udp_server.py
@@ -7,21 +7,44 @@ from io import StringIO
 
 addr = "localhost"
 port = 9090
-data_len = 5000 
-response_msg = ''.join(random.choices(string.ascii_uppercase, k = data_len))
+client_mtu = 200
+header_len = 20
+
+data_len = 1024
+BIG_MESSAGE = ''.join(random.choices(string.ascii_uppercase, k = data_len))
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind((addr, port))
+
+# Each fragment will have following fields
+# ID
+# Offset
+# MF
+# Header len
+# Total len
 
 while True:
     msg, client = server.recvfrom(1024)
     ads, prt = client
     print(f"Received Request from {ads}:{prt}")
-    str = StringIO(response_msg)
-    chunk = str.read(512)
+    id = int(random.random() * 1000)
+    offset = 0
+    mf = 1
+    str = StringIO(BIG_MESSAGE)
+    read_len = client_mtu - header_len
+    chunk = str.read(read_len)
     while chunk:
-        server.sendto(chunk.encode('utf-8'), (ads, prt))
-        chunk = str.read(512)
+        chunk = str.read(read_len)
+        total_len = header_len + len(chunk);
+        if not chunk:
+            mf = 0
+        header = f"{id};{offset};{mf};{header_len};{total_len}"
+        header += " " * (header_len - len(header))
+        data = header + ";" + chunk
+        server.sendto(data.encode('utf-8'), (ads, prt))
+        if chunk:
+            offset += total_len
+            total_len = header_len + len(chunk)
     # after all the data is send we'll send
     # a trailer message to stop listening
     # for new message
